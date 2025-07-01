6705f6944dde50443bc3b57166e49b7babac2490
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2025-07-01 23:20:02 +0300

    chore(docs): create a readme markdown file for the project

--- src/components/icons/Search.tsx
+++ src/components/icons/Search.tsx
@@ -3,7 +3,20 @@ import { SVGProps } from "react";
 
 const Search = (props: SVGProps<SVGSVGElement>) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className={`lucide lucide-search ${props.className || ""}`} {...props}><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>  
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`lucide lucide-search ${props.className || ""}`}
+      {...props}
+    >
+      {" "}
+      <path d="M23 23L17 17" stroke="currentColor" />{" "}
+      <path
+        d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+        stroke="currentColor"
+      />{" "}
+    </svg>
   );
 };
 
